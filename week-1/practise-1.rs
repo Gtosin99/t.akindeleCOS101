ec34b17ebb7eae27284f29df869b017273d73071
t.akindeleCOS101 <tosinakindele826@gmail.com>
2023-10-20 00:48:49 +0100

    (My first commit)

--- week-1/practise-1.rs
+++ week-1/practise-1.rs
@@ -1,12 +1,12 @@
 fn main() {
-     println!("Welcome to COS101!")
+     println!("Welcome to COS101!");
 
-     println!("The course learning outcome is as follows")
+     println!("The course learning outcome is as follows");
 
-     println!("  1. Distinguish between computer and computer programing,/fn
-     	       2. Develop some techniques in computer science,/n
-     	       3. Understand the different areas of study in computer science,/n
-     	       4. Conversant with applications of computer science,/n
-     	       5. Conversant with computer programming concepts./n
-     	       6. Navigate the career prospects in computer science.")
+     println!("1. Distinguish between computer and computer programing,/n
+               2. Develop some techniques in computer science,/n
+               3. Understand the different areas of study in computer science,/n
+               4. Conversant with applications of computer science,/n
+               5. Conversant with computer programming concepts./n
+               6. Navigate the career prospects in computer science.")
 }
---
+++ week-2/practise-1.rs
@@ -0,0 +1,4 @@
+fn main() {
+	println!("Welcome to rust programming");
+	println!("Henceforth you shall be called RUSTACIANS")
+}
