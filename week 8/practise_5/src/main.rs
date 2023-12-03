c5f575d952b1c4d889f8bd6dbf1d2031c2cdff44
t.akindeleCOS101 <tosinakindele826@gmail.com>
2023-12-03 20:53:36 +0100

    (My Eight commit)

--- week 8/practise_5/src/main.rs
+++ week 8/practise_5/src/main.rs
@@ -12,10 +12,11 @@ let mut  input1 =  String::new();
 
 println!("How many cities do you want to enter");
 std::io::stdin().read_line(&mut input1).expect("Failed to read input");
-
 let city_num:i32 =input1.trim().parse().expect("Inalid Input");
 for count in 0..city_num {
     let mut input2 = String::new();
+    println!("Enter city {}",count+1 );
+    std::io::stdin().read_line(&mut input2).expect("Failed to read input");
 let new_city:String = input2.trim().parse().expect("Invalid input"); 
 city.push(new_city);
 }
